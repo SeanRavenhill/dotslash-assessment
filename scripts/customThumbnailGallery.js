6da8312d5b1d58be5aeaca3f8388c9c8a37a00d2
SeanRavenhill <seanravenhill@gmail.com>
2021-11-18 00:08:03 +0200

    carousel and control via thumbails of carousel as well as active, and hover states working
    made a custom JS script to handle the behaviour of the active states on the thumbnail gallery. I could not find in the docs a way to do this with Bootstrap utilies or components, nor did I have much luck google searching for solutions. Next I need to hook up the carousel state to the thumb galleries, so when you navigate via the carousel, the active states on the thumbnails is dynamic

---
+++ scripts/customThumbnailGallery.js
@@ -0,0 +1,37 @@
+const thumbBtn1 = document.getElementById("thumb-btn-1");
+const thumbBtn2 = document.getElementById("thumb-btn-2");
+const thumbBtn3 = document.getElementById("thumb-btn-3");
+const thumbBtn4 = document.getElementById("thumb-btn-4");
+const thumbBtn5 = document.getElementById("thumb-btn-5");
+
+const thumbBtnArray = [
+    thumbBtn1,
+    thumbBtn2,
+    thumbBtn3,
+    thumbBtn4,
+    thumbBtn5
+];
+
+window.onload = thumbBtnGallery;
+
+// function thumbBtnGallery() {
+//   document.getElementById("thumbnail-btn-gallery").onclick = clicked;
+// };
+
+function thumbBtnGallery() {
+    thumbBtn1.onclick = clicked;
+    thumbBtn2.onclick = clicked;
+    thumbBtn3.onclick = clicked;
+    thumbBtn4.onclick = clicked;
+    thumbBtn5.onclick = clicked;
+};
+
+function clicked(e) {
+    let thumbBtnActiveToggle = e.target.classList
+    for (const thumbBtn of thumbBtnArray) {
+        if (thumbBtn.className === "custom-carousel-thumb-btn custom-active") {
+            thumbBtn.classList.remove("custom-active")
+        };
+    };
+    thumbBtnActiveToggle.add("custom-active");
+};
